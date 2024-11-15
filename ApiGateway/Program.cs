b83500717af4203c636bdc2dd73d5ee17579e58d
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-15 14:47:46 -0500

    feat: adding auth methods to gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -10,15 +10,20 @@ builder.Configuration.AddJsonFile("ocelot.json");
 const string authenticationProviderKey = "BearerAuth";
 var key = Encoding.ASCII.GetBytes("SECRET-JWT-KEY");
 
+// JWT Settings
+var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettingsSection.GetValue<string>("SecretKey");
+
 builder.Services.AddAuthentication(authenticationProviderKey)
     .AddJwtBearer(authenticationProviderKey, options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            ValidateAudience = false,
             ValidateLifetime = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key)
+            ValidIssuer = "javefamilia.org",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey))
         };
     });
 
