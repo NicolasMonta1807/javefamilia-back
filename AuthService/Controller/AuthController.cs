b16fa958496c4a452c6d9aad2d5e88a213c2342a
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-15 11:52:39 -0500

    init: setting up authentication

---
+++ AuthService/Controller/AuthController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthService.Model;
+
+namespace AuthService.Controller;
+
+[ApiController]
+[Route("/auth")]
+public class AuthController : ControllerBase
+{
+    private const string SecretKey = "";
+    private const string Issuer = "http://localhost:5020";
+    private const string Audience = "YourAudience";
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request.Username == "admin" && request.Password == "password")
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username),
+                    new Claim(ClaimTypes.Role, "admin")
+                }),
+                Expires = DateTime.UtcNow.AddHours(24),
+                SigningCredentials =
+                    new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = Issuer,
+                Audience = Audience
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { Token = tokenString });
+        }
+
+        return Unauthorized();
+    }
+}
