0f0d6d514e00863edc24227195828a2d95d8d25a
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-15 14:23:36 -0500

    feat: adding non-affiliated register and encrypting

---
+++ AuthService/Model/UserModel/DTOs/RegisterUserDto.cs
@@ -0,0 +1,15 @@
+using AuthService.Model.Enum;
+
+namespace AuthService.Model.DTOs;
+
+public class RegisterUserDto
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required TipoDocumento TipoDocumento { get; set; }
+    public required string Document { get; set; }
+}
