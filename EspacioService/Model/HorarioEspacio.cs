e88ad1662515eb1be84cd181ee438863db8b6707
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-16 18:41:49 -0500

    feat: adding keyvault secrets to Espacios

--- EspacioService/Model/HorarioEspacio.cs
+++ EspacioService/Model/HorarioEspacio.cs
@@ -1,16 +1,15 @@
-using System;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-namespace EspacioService.Model
+namespace EspacioService.Model;
+
+public class HorarioEspacio
 {
-    public class HorarioEspacio
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
-        public bool Availavility { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
-    }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public bool Availavility { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
 }
