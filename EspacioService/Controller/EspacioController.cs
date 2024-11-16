5f2a7cf7c8f5cc1ddb94ad4afc1771dce47ccc78
NicolasARDBE <na.reyesd@javeriana.edu.co>
2024-11-16 10:53:55 -0500

    feat: adding EspacioService routes to gateway

--- EspacioService/Controller/EspacioController.cs
+++ EspacioService/Controller/EspacioController.cs
@@ -33,14 +33,14 @@ namespace EspacioService.Controller
         }
 
         [HttpPost]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> CreateEspacio([FromBody] Espacio espacio)
         {
             await _espacioService.CreateEspacioAsync(espacio);
             return Ok();
         }
         [HttpPost("/addHorario/{id}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> AddHorario(string id, [FromBody] HorarioEspacio horario)
         {
             await _espacioService.AddHorarioAsync(id, horario);
@@ -48,14 +48,14 @@ namespace EspacioService.Controller
         }
 
         [HttpPut("{id}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> UpdateEspacio(string id, [FromBody] Espacio espacio)
         {
             await _espacioService.UpdateEspacioAsync(id, espacio);
             return Ok();
         }
         [HttpPut("/updateHorario/{id}/{idHorario}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> UpdateHorario(string id, string idHorario, [FromBody] HorarioEspacio horario)
         {
             await _espacioService.UpdateHorarioAsync(id, idHorario, horario);
@@ -63,7 +63,7 @@ namespace EspacioService.Controller
         }
 
         [HttpDelete("{id}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> DeleteEspacio(string id)
         {
             await _espacioService.DeleteEspacioAsync(id);
@@ -71,7 +71,7 @@ namespace EspacioService.Controller
         }
 
         [HttpDelete("/deleteHorario/{id}/{idHorario}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> DeleteHorario(string id, string idHorario)
         {
             await _espacioService.DeleteHorarioAsync(id, idHorario);
