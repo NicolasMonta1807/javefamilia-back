a089cac9fcb7822aee413e48af616a52349548a8
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-17 22:16:47 -0500

    feat: adding docker swarm deployment

---
+++ docker-build.sh
@@ -0,0 +1,4 @@
+docker build -t api-gateway:latest ./ApiGateway
+docker build -t auth-service:latest ./AuthService
+docker build -t espacios-service:latest ./EspacioService
+docker build -t sedes-service:latest ./SedesService
