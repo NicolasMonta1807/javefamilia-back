8068322d4486c2210ed176139d0569543e2f98d1
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-11-18 01:46:46 -0500

    fix: cloud adjustments

--- docker-build.sh
+++ docker-build.sh
@@ -2,3 +2,8 @@ docker build -t nicolasmonta1807/api-gateway:latest ./ApiGateway
 docker build -t nicolasmonta1807/auth-service:latest ./AuthService
 docker build -t nicolasmonta1807/espacios-service:latest ./EspacioService
 docker build -t nicolasmonta1807/sedes-service:latest ./SedesService
+
+docker push nicolasmonta1807/api-gateway
+docker push nicolasmonta1807/auth-service
+docker push nicolasmonta1807/espacios-service
+docker push nicolasmonta1807/sedes-service
